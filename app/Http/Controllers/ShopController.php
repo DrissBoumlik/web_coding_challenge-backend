f56326a1d189ce66a72bb3a15a31a57ce259a258
Driss Boumlik <boumlik.t@gmail.com>
2018-12-20 11:44:34 +0100

    FEATURE: Adding Controllers

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Shop;
+use Illuminate\Http\Request;
+use Auth;
+
+class ShopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+        $user_id = $user->id;
+        $location = $user->user_location;
+        $shops = Shop::whereNotIn('id', function($query) use($user_id){
+            $query->select('shop_id')
+                  ->from('shop_users')
+                  ->whereNotNull('is_liked')
+                  ->where('user_id', $user_id);
+        })->get();
+        $shops = Shop::byDistance($location, $shops); // Getting all the shops sorted by distance
+        $shops = $this->paginate($shops, 12);
+        return response()->json(compact('shops'));
+    }
+}
---
+++ app/Http/Controllers/ShopUserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ShopUser;
+use Auth;
+
+class ShopUserController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user_id = Auth::user()->id;
+
+        $shop_user = ShopUser::where('user_id',$user_id)->where('shop_id',$request->shop_id)->first();
+
+        if($shop_user != null) {
+            $shop_user['is_liked'] = $request->is_liked;
+            //return self::update($request, $shop_user);
+        }
+        else{
+            $shop_user = new ShopUser();
+            $shop_user->fill([
+                'user_id'   => $user_id,
+                'shop_id'   => $request->shop_id,
+                'is_liked'   => $request->is_liked
+            ]);
+        }
+        $shop_user->save();
+        if($request->is_liked == 0)
+            DeleteDislikedShop::dispatch($shop_user)->delay(now()->addSeconds(10));
+        return response()->json([
+            'shop' => $shop_user,
+            'message' => ($request->is_liked) ? 'Shop Liked' : 'Shop Disliked'
+        ]);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($shop_id)
+    {
+        $shop_user = ShopUser::where('user_id', Auth::user()->id)->where('shop_id',$shop_id)->first();
+        if ($shop_user->delete()) {
+            return response()->json([
+                'shop' => $shop_user,
+                'message' => 'Successfully removed from your favorite list!'
+            ]);
+        }
+        else {
+            return response()->json([
+                'shop' => $shop_user,
+                'message' => 'Something went wrong!'
+            ]);
+        }
+    }
+
+    public function liked_shopes(){
+        $user = Auth::user(); // For testing
+        $shops = $user->shops()->where('is_liked', 1)->paginate(12);
+        return response()->json(compact('shops'));
+    }
+}
